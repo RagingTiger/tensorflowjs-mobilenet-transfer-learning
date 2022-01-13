9de4241567ca065f2be373713eac940f970fb8f2
Glitch (hello-webpage) <none>
2022-01-13 00:57:48 +0000

    🍴👟 Checkpoint ./README.md:9162534/2052 ./index.html:9162534/103 ./script.js:9162534/140

--- script.js
+++ script.js
@@ -15,7 +15,4 @@
  * =============================================================================
  */
 
-const status = document.getElementById('status');
-if (status) {
-  status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
-}
+
