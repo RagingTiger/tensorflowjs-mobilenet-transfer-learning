06f493db36a9d1f32aacdd812df021b908dd8a8b
Glitch (hello-webpage) <none>
2022-01-18 01:31:12 +0000

    📇📙 Checkpoint ./script.js:9162534/1904

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ let trainingDataOutputs = [];
 let examplesCount = [];
 let predict = false;
 let mobileNetBase = undefined;
+let model = tf.sequential();
 
 
 function customPrint(line) {
@@ -79,26 +80,25 @@ async function loadMobileNetFeatureModel() {
   });
 }
 
-loadMobileNetFeatureModel().then(function);
+loadMobileNetFeatureModel().then(function() {
+  model.add(tf.layers.flatten({inputShape: mobileNetBase.output.shape.slice(1)}));
+  model.add(tf.layers.dense({units: 128, activation: 'relu'}));
+  model.add(tf.layers.dense({units: CLASS_NAMES.length, activation: 'softmax'}));
 
+  model.summary();
 
-let model = tf.sequential();
-model.add(tf.layers.flatten({inputShape: mobileNetBase.output.shape}));
-model.add(tf.layers.dense({units: 128, activation: 'relu'}));
-model.add(tf.layers.dense({units: CLASS_NAMES.length, activation: 'softmax'}));
-
-model.summary();
-
-// Compile the model with the defined optimizer and specify a loss function to use.
-model.compile({
-  // Adam changes the learning rate over time which is useful.
-  optimizer: 'adam',
-  // Use the correct loss function. If 2 classes of data, must use binaryCrossentropy.
-  // Else categoricalCrossentropy is used if more than 2 classes.
-  loss: (CLASS_NAMES.length === 2) ? 'binaryCrossentropy': 'categoricalCrossentropy', 
-  // As this is a classification problem you can record accuracy in the logs too!
-  metrics: ['accuracy']  
+  // Compile the model with the defined optimizer and specify a loss function to use.
+  model.compile({
+    // Adam changes the learning rate over time which is useful.
+    optimizer: 'adam',
+    // Use the correct loss function. If 2 classes of data, must use binaryCrossentropy.
+    // Else categoricalCrossentropy is used if more than 2 classes.
+    loss: (CLASS_NAMES.length === 2) ? 'binaryCrossentropy': 'categoricalCrossentropy', 
+    // As this is a classification problem you can record accuracy in the logs too!
+    metrics: ['accuracy']  
+  });
 });
+
 
 
 /**
