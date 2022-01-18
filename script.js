4e4f476897c812d89da24525197f86b499b5ada4
Glitch (hello-webpage) <none>
2022-01-18 01:57:04 +0000

    🏥📰 Checkpoint ./script.js:9162534/1797

--- script.js
+++ script.js
@@ -50,7 +50,6 @@ let trainingDataOutputs = [];
 let examplesCount = [];
 let predict = false;
 let mobileNetBase = undefined;
-let model = tf.sequential();
 
 
 function customPrint(line) {
@@ -80,23 +79,25 @@ async function loadMobileNetFeatureModel() {
   });
 }
 
-loadMobileNetFeatureModel().then(function() {
-  model.add(tf.layers.flatten({inputShape: mobileNetBase.output.shape.slice(1)}));
-  model.add(tf.layers.dense({units: 128, activation: 'relu'}));
-  model.add(tf.layers.dense({units: CLASS_NAMES.length, activation: 'softmax'}));
+loadMobileNetFeatureModel();
 
-  model.summary();
 
-  // Compile the model with the defined optimizer and specify a loss function to use.
-  model.compile({
-    // Adam changes the learning rate over time which is useful.
-    optimizer: 'adam',
-    // Use the correct loss function. If 2 classes of data, must use binaryCrossentropy.
-    // Else categoricalCrossentropy is used if more than 2 classes.
-    loss: (CLASS_NAMES.length === 2) ? 'binaryCrossentropy': 'categoricalCrossentropy', 
-    // As this is a classification problem you can record accuracy in the logs too!
-    metrics: ['accuracy']  
-  });
+let model = tf.sequential();
+model.add(tf.layers.flatten({inputShape: [7, 7, 256]}));
+model.add(tf.layers.dense({units: 128, activation: 'relu'}));
+model.add(tf.layers.dense({units: CLASS_NAMES.length, activation: 'softmax'}));
+
+model.summary();
+
+// Compile the model with the defined optimizer and specify a loss function to use.
+model.compile({
+  // Adam changes the learning rate over time which is useful.
+  optimizer: 'adam',
+  // Use the correct loss function. If 2 classes of data, must use binaryCrossentropy.
+  // Else categoricalCrossentropy is used if more than 2 classes.
+  loss: (CLASS_NAMES.length === 2) ? 'binaryCrossentropy': 'categoricalCrossentropy', 
+  // As this is a classification problem you can record accuracy in the logs too!
+  metrics: ['accuracy']  
 });
 
 
@@ -171,7 +172,7 @@ function dataGatherLoop() {
   if (videoPlaying && gatherDataState !== STOP_DATA_GATHER) {
     // Ensure tensors are cleaned up.
     let imageFeatures = calculateFeaturesOnCurrentFrame();
-
+    imageFeatures.print();
     trainingDataInputs.push(imageFeatures);
     trainingDataOutputs.push(gatherDataState);
     
